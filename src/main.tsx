3621b561f1f0e5a6cb9ec6833b81d887a51bbc6e
Joao Pedro <jpmrc49@gmail.com>
2023-04-29 15:02:55 -0300

    add a store

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { Provider } from 'react-redux';
 
+import { store } from './redux/SignUp/store.ts';
+// eslint-disable-next-line prettier/prettier
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
