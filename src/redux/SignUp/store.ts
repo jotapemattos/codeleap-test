3c438d1b6856f9d70d5b2ddd180d2d0038189e39
Joao Pedro <jpmrc49@gmail.com>
2023-04-29 17:30:50 -0300

    change the store name

--- src/redux/SignUp/store.ts
+++ src/redux/SignUp/store.ts
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { nameReducer } from './name-slice';
 
-export const store = configureStore({
+export const storeName = configureStore({
   reducer: {
     name: nameReducer
   }
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof storeName.getState>;
