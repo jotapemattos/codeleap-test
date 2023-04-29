904a471164db7469781a4f900a61b07fcfde6bbb
Joao Pedro <jpmrc49@gmail.com>
2023-04-29 17:32:37 -0300

    create a custom hook for useSelectorPosts

--- src/hooks/useAppSelector.ts
+++ src/hooks/useAppSelector.ts
@@ -1,4 +1,6 @@
 import { useSelector, TypedUseSelectorHook } from 'react-redux';
 import { RootState } from '../redux/SignUp/store';
+import { RootPostState } from '../redux/FormPosts/store';
 
-export const useCustomSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useNameSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const usePostSelector: TypedUseSelectorHook<RootPostState> = useSelector;
