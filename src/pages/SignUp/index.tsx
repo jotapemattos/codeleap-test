aff9fde7a2e2eb4847266221be714f19c7477927
Joao Pedro <jpmrc49@gmail.com>
2023-04-29 11:41:38 -0300

    built the signup page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const index = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState('');
+
+  const isUserValid = user.trim() !== '';
+
+  const handleClick = () => {
+    setUser('');
+    navigate('/posts');
+  };
+
+  return (
+    <div className="bg-white w-[500px] h-[205px] flex flex-col gap-6 border border-[#CCCCCC] rounded-2xl p-6">
+      <h1 className="font-bold text-2xl">Welcome to CodeLeap network!</h1>
+      <span className="flex flex-col gap-4 items-end">
+        <form className="w-full flex flex-col gap-2 text-base">
+          <label>Please enter your username</label>
+          <input
+            type="text"
+            className="outline-none border border-[#777777] rounded-lg h-8 p-2 text-sm"
+            placeholder="John Doe"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+        </form>
+        <button
+          type="submit"
+          className={
+            !user
+              ? `w-28 h-8 rounded-lg bg-[#7695EC] text-white opacity-40`
+              : `w-28 h-8 rounded-lg bg-[#7695EC] text-white`
+          }
+          onClick={() => handleClick()}
+          disabled={!isUserValid}
+        >
+          ENTER
+        </button>
+      </span>
+    </div>
+  );
+};
+
+export default index;
