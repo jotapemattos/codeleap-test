e0dcd6181c6c4070bba177bcc06b07e0c6d78d51
Joao Pedro <jpmrc49@gmail.com>
2023-04-29 11:42:12 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
 import './styles/globals.css';
+import FormPosts from './pages/FormPosts';
 
 function App() {
-  return <div className="bg-[#DDDDDD] min-w-screen min-h-screen">a</div>;
+  return (
+    <div className="bg-[#DDDDDD] min-w-screen min-h-screen flex items-center justify-center">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/posts" element={<FormPosts />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
