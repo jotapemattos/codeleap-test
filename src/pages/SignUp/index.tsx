f818e6a2afb69c72530b131482dae13aa93e3c4a
Joao Pedro <jpmrc49@gmail.com>
2023-04-29 17:31:53 -0300

    fix the username issue

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,10 @@
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { setName } from '../../redux/SignUp/name-slice';
-import { useCustomSelector } from '../../hooks/useAppSelector';
+import { useNameSelector } from '../../hooks/useAppSelector';
 
 const index = () => {
-  const user = useCustomSelector((state) => state.name.value);
+  const user = useNameSelector((state) => state.name.value);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -12,7 +12,6 @@ const index = () => {
 
   const handleClick = () => {
     navigate('/posts');
-    dispatch(setName(''));
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,14 +19,14 @@ const index = () => {
   };
 
   return (
-    <div className="bg-white w-[500px] h-[205px] flex flex-col gap-6 border border-[#CCCCCC] rounded-2xl p-6">
+    <div className="bg-white w-[500px] h-[205px] flex flex-col gap-6 border border-thirdGray rounded-2xl p-6">
       <h1 className="font-bold text-2xl">Welcome to CodeLeap network!</h1>
       <span className="flex flex-col gap-4 items-end">
         <form className="w-full flex flex-col gap-2 text-base">
           <label>Please enter your username</label>
           <input
             type="text"
-            className="outline-none border border-[#777777] rounded-lg h-8 p-2 text-sm"
+            className="outline-none border border-secondGray rounded-lg h-8 p-2 text-sm"
             placeholder="John Doe"
             value={user}
             onChange={handleChange}
@@ -37,8 +36,8 @@ const index = () => {
           type="submit"
           className={
             !user
-              ? `w-28 h-8 rounded-lg bg-[#7695EC] text-white opacity-40`
-              : `w-28 h-8 rounded-lg bg-[#7695EC] text-white`
+              ? `w-28 h-8 rounded-lg bg-mainBlue text-white opacity-40`
+              : `w-28 h-8 rounded-lg bg-mainBlue text-white`
           }
           onClick={() => handleClick()}
           disabled={!isUserValid}
