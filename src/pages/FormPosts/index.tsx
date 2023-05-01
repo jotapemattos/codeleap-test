bce03299ab4f471b0f58b1f003b3ece55e3326e5
Joao Pedro <jpmrc49@gmail.com>
2023-05-01 01:51:46 -0300

    built the posts part

---
+++ src/pages/FormPosts/index.tsx
@@ -0,0 +1,16 @@
+import Form from '../../components/FormPosts/Form';
+import PostsCards from '../../components/FormPosts/PostsCards';
+
+const FormPosts = () => {
+  return (
+    <div className="w-[800px] min-h-screen flex flex-col items-center gap-6 bg-white pb-6">
+      <header className="w-full h-20 bg-mainBlue flex items-center px-9">
+        <h1 className="text-xl font-bold text-white">CodeLeap Network</h1>
+      </header>
+      <Form />
+      <PostsCards />
+    </div>
+  );
+};
+
+export default FormPosts;
