6825f46de48d588bfad8861de7b3ea7262c01e6f
Joao Pedro <jpmrc49@gmail.com>
2023-05-01 21:28:51 -0300

    create a hook for components

---
+++ src/hooks/FormPosts/useForm.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { postPosts } from '../../actions/service';
+import { setPost } from '../../redux/FormPosts/posts-slice';
+import { useNameSelector } from '../useAppSelector';
+
+export const useForm = () => {
+  const username = useNameSelector((state) => state.name.value);
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+  const created_datetime = today.toISOString();
+
+  const isPostValid = title.trim() !== '' && content.trim() !== '';
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const post = {
+      username,
+      created_datetime,
+      title,
+      content
+    };
+    dispatch(setPost(post));
+    postPosts(post);
+    setTitle('');
+    setContent('');
+  };
+
+  return {
+    isPostValid,
+    handleSubmit,
+    title,
+    setTitle,
+    content,
+    setContent,
+    username
+  };
+};
---
+++ src/hooks/FormPosts/usePostsCards.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Data, getPosts } from '../../actions/service';
+import { useNameSelector } from '../useAppSelector';
+
+export const usePostsCards = () => {
+  const username = useNameSelector((state) => state.name.value);
+  const [data, setData] = useState<Data[] | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [deleteId, setDeleteId] = useState(0);
+  const [updateId, setUpdateId] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const now = new Date().toISOString();
+  useEffect(() => {
+    getPosts(setData);
+  }, [data]);
+
+  const newData = data?.map((item) => {
+    const minutesAgo =
+      new Date(now).getTime() - new Date(item.created_datetime).getTime();
+    const roundedMinutesAgo = Math.floor(minutesAgo / 60000);
+    return { ...item, minutes_ago: roundedMinutesAgo };
+  });
+
+  const filterIdToDelete = (id: number) => {
+    setDeleteId(id);
+    setIsOpen(true);
+    setIsDeleting(true);
+  };
+
+  const filterIdToUpdate = (id: number) => {
+    setUpdateId(id);
+    setIsOpen(true);
+    setIsDeleting(false);
+  };
+
+  return {
+    filterIdToDelete,
+    filterIdToUpdate,
+    setIsOpen,
+    setDeleteId,
+    setUpdateId,
+    username,
+    newData,
+    deleteId,
+    updateId,
+    isDeleting,
+    isOpen
+  };
+};
