00ae088fece63cbc43bbf752deb307589e83d991
Joao Pedro <jpmrc49@gmail.com>
2023-05-01 21:31:40 -0300

    create the delete and update functions

--- src/actions/service.ts
+++ src/actions/service.ts
@@ -9,6 +9,11 @@ export interface Data {
   content: string;
 }
 
+interface UpdatePostProps {
+  title: string;
+  content: string;
+}
+
 interface DataProps {
   setData: (data: Data[]) => void;
 }
@@ -21,4 +26,12 @@ export const getPosts = async (setData: DataProps['setData']) => {
   await axios
     .get('https://dev.codeleap.co.uk/careers/?format=json&limit=10&offset=0')
     .then((response) => setData(response.data.results));
+};
+
+export const deletePost = async (id: number) => {
+  await axios.delete(`https://dev.codeleap.co.uk/careers/${id}/`);
+};
+
+export const updatePost = async (id: number, post: UpdatePostProps) => {
+  await axios.patch(`https://dev.codeleap.co.uk/careers/${id}/`, post);
 };
