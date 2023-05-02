44884ce811e3407d1e784ede2aa6b9f2e5a3b6d5
Joao Pedro <jpmrc49@gmail.com>
2023-05-01 21:29:33 -0300

    create a hook for the posts part

--- src/hooks/FormPosts/usePostsCards.tsx
+++ src/hooks/FormPosts/usePostsCards.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Data, getPosts } from '../../actions/service';
 import { useNameSelector } from '../useAppSelector';
 
